be1a9cc33de26012cd29fb9530207b37f5cbf13e
Joseph Lawhorn <jlawhorn@degdigital.com>
2013-01-21 21:43:44 -0600

    Added sisyphus.

--- Scripts/calculate.js
+++ Scripts/calculate.js
@@ -12,7 +12,26 @@ $(document).ready(function(){
 		statIn = 5,
 		statAg = 5,
 		statLk = 5,
-		statPool = 5;
+		statPool = 5,
+		smallGuns = 0,
+		bigGuns = 0,
+		energyWeapons = 0,
+		unarmed = 0,
+		meleeWeapons = 0,
+		throwingWeapons = 0,
+		firstAid = 0,
+		doctor = 0,
+		sneak = 0,
+		lockpick = 0,
+		steal = 0,
+		traps = 0,
+		science = 0,
+		repair = 0,
+		pilot = 0,
+		speech = 0,
+		barter = 0,
+		gambling = 0,
+		outdoorsman = 0;
 
 		initializeValues();
 
@@ -45,36 +64,71 @@ $(document).ready(function(){
 		}
 	}
 
-	function setStatPointsFromDom() {
-		var currentTotalStats = 0,
-		statSt = $('#strength');
-		statPe = $('#perception');
-		statEn = $('#endurance');
-		statCh = $('#charisma');
-		statIn = $('#intellegence');
-		statAg = $('#agility');
-		statLk = $('#luck');
-		statPool = $('#statPool');
-		currentTotalStats = statSt + statPe + statEn + statCh + statIn + statAg + statLk + statPool;
-		if (validateStats(currentTotalStats)) {
-
-		}
+	function logStats() {
+		console.log('str ' + statSt);
+		console.log('per ' + statPe);
+		console.log('end ' + statEn);
+		console.log('cha ' + statCh);
+		console.log('int ' + statIn);
+		console.log('agi ' + statAg);
+		console.log('luk ' + statLk);
 	}
 
-	function validateStats(currentTotalStats) {
+	function setStatPointsFromDom() {
+		statSt = parseInt($('#strength').val());
+		statPe = parseInt($('#perception').val());
+		statEn = parseInt($('#endurance').val());
+		statCh = parseInt($('#charisma').val());
+		statIn = parseInt($('#intellegence').val());
+		statAg = parseInt($('#agility').val());
+		statLk = parseInt($('#luck').val());
+		balanceStatPool();
+	}
+
+	function setStatPointsFromVars() {
+		$('#strength').val(statSt);
+		$('#perception').val(statPe);
+		$('#endurance').val(statEn);
+		$('#charisma').val(statCh);
+		$('#intellegence').val(statIn);
+		$('#agility').val(statAg);
+		$('#luck').val(statLk);
+		balanceStatPool();
+	}
+
+	function validateStats() {
+		var currentTotalStats = statSt + statPe + statEn + statCh + statIn + statAg + statLk + statPool;
 		if (currentTotalStats == totalStats) {
 			return true;
 		} else {
 			return false;
 		}
-		
 	}
 
-	function changeStats (target) {
-		var $target = $(target);
-		var newStatVal = $target.val();
-		if (newStatVal > 10 || newStatVal < 1) {
-			$target.addClass('error');
+	function balanceStatPool() {
+		statPool = totalStats - (statSt + statPe + statEn + statCh + statIn + statAg + statLk);
+		if (statPool < 0) {
+			$('#statPool').val(statPool);
+			return false;
+		} else {
+			$('#statPool').val(statPool);
+			return true;
 		}
+
+	}
+
+	function changeStats(target) {
+		var $target = $(target);
+		var newStatVal = $target.val(),
+			attributeName = $target.data('attrib');
+		if (newStatVal > 10 || newStatVal < 1) {
+			setStatPointsFromVars();
+		} else {
+			setStatPointsFromDom();
+		}
+	}
+
+	function calcSmallGuns () {
+
 	}
 });
